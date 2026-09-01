ba75f1fea8fa2c3c851d9b5b037a4e0f9fafe8c6
Dina <dnguye41@syr.edu>
2026-09-01 14:37:58 -0400

    Correct password check and user feedback
    Fix password input and success condition logic.

--- 1-python/1-2-1a.py
+++ 1-python/1-2-1a.py
@@ -1,10 +1,15 @@
 PASSWORD = "secret" #Constant - Holds a fixed configuration, key, or default value that shouldn't change while the script runs.
-
+success = False #Not a capital SUCCESS 
 for attempt on range(5):
-  your_password = input("Enter the password:") #True variable - Holds dynamic data that changes based on user input, program state, or calculations.
+  your_password = input("Enter the password: ") #True variable - Holds dynamic data that changes based on user input, program state, or calculations.
   if your_password = PASSWORD:
     print("Access granted:")
+    success = True
     break #Stops an iteration when you enter the password
   else:
       print("Access denied")
+    
 print(f"Attempt {attempt + 1} of 5")
+
+if not success:
+    print("You are locked out")
