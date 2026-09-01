e501ca3dc7d66e258b608831aaa347f7fbc201c4
Dina <dnguye41@syr.edu>
2026-09-01 14:32:09 -0400

    Refactor password input to allow 5 attempts
    Updated password check logic to allow multiple attempts.

--- 1-python/1-2-1a.py
+++ 1-python/1-2-1a.py
@@ -1,7 +1,8 @@
-PASSWORD = "secret"
-your_password = input("Enter the password:")
-if your_password = PASSWORD:
-  print("Access granted:")
-else:
-    print("Access denied")
-  exit(1)
+PASSWORD = "secret" #Constant - Holds a fixed configuration, key, or default value that shouldn't change while the script runs.
+
+for attempt on range(5):
+  your_password = input("Enter the password:") #True variable - Holds dynamic data that changes based on user input, program state, or calculations.
+  if your_password = PASSWORD:
+    print("Access granted:")
+  else:
+      print("Access denied")
