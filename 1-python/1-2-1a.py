c5b8dc564d735b1bf472fee6c6dc91d51279a3f2
Dina <dnguye41@syr.edu>
2026-09-01 14:34:24 -0400

    Correct password input loop and condition
    Fix syntax errors in password check loop.

--- 1-python/1-2-1a.py
+++ 1-python/1-2-1a.py
@@ -4,5 +4,7 @@ for attempt on range(5):
   your_password = input("Enter the password:") #True variable - Holds dynamic data that changes based on user input, program state, or calculations.
   if your_password = PASSWORD:
     print("Access granted:")
+    break #Stops an iteration when you enter the password
   else:
       print("Access denied")
+print(f"Attempt {attempt + 1} of 5")
